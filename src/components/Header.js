e9e8958941837665ea9df31f1d2ea535d7962bb3
lokhandecoder <amitlokhande909@gmail.com>
2023-07-27 23:06:19 +0530

    added changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,9 +19,9 @@ export default class Header extends Component {
             </ul>
          </nav>
 
-         <div className="row banner">
-            <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}</h1>
+         <div className="row banner animate__rubberBand">
+            <div className="banner-text  ">
+               <h1 className="responsive-headline ">I am {resumeData.name}</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
                </h3>
                <hr/>
