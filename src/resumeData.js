83f422d9bb818f6beae2dc128b41ebac6a7ea277
lokhandecoder <amitlokhande909@gmail.com>
2023-07-27 13:42:43 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name":"Amit Lokhande",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/amit-lokhande-971154177/",
+    "skypeid": "Your skypeid",
+    "roleDescription":"Highly motivated and skilled Full Stack Developer with one year of hands-on experience in designing and implementing end-to-end web applications.Data enthusiast with a strong interest in leveraging data-driven solutions to enhance user experiences and drive business growth. Proficient in both front-end and back-end technologies, adept at collaborating  with cross-functional teams to deliver exceptional user experiences ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/amit-lokhande-971154177/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/lokhandecoder",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Highly motivated and skilled Full Stack Developer with one year of hands-on experience in designing and implementing end-to-end web applications.Data enthusiast with a strong interest in leveraging data-driven solutions to enhance user experiences and drive business growth. Proficient in both front-end and back-end technologies, adept at collaborating  with cross-functional teams to deliver exceptional user experiences ",
+    "address":"India",
+    "website":"https://lokhandecoder.github.io",
+    "education":[
+      {
+        "UniversityName":"Vidyalankar Institute of Technology",
+        "specialization":"Electronics & Telecommunication Engineering",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2022",
+        "Achievements":"Face Mask Detection Door Lock using Python with CV2 library by Raspberry Pi 3B+ which was successfully operating project and used in my organizations. \t IIG (Institute of Geomagnetisms) with the part of this tremendous team helped to create a minor project for ISRO which is under process. This project was to find the signal at a altitude of 25Km from earth surface which is implemented on big size gallons. Lab testing is done and tested but ground testing is remaining to complete."
+      },
+      {
+        "UniversityName":"Shah & Anchor Kutchhi Polyrechnic",
+        "specialization":"Electronics & Telecommunication Engineering",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "Achievements": "Water Level Controller using NodeMCU v3 and ultrasonic sensor which will detect the water level and show the real time data on website as well as mobile app which can be control using user by anywhere."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Researchwire Knowledge Solutions",
+        "specialization":"Associate Software Engineer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2022",
+        "Achievements":"In current company, successfully delivering two projects spanning diverse technologies. In Project 1 (Researchwire Official website), demonstrated expertise in HTML, CSS, jQuery, JavaScript, and React to create modern and interactive front-end components. For Project 2 (Frand Avenue), skillfully utilized HTML, CSS, jQuery, PHP, Symfony, and MySQL to build robust and scalable back-end systems. A data enthusiast with a keen interest in leveraging data-driven insights to enhance application performance and user experiences"
+      },
+      {
+        "CompanyName":"Wonderbiz Technologies Pvt Ltd",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2023",
+        "Achievements":"Recently Join wonderkiz as a full stack developer which helped to use my front-end skills where I am working on Schneider Electric Project."
+      }
+    ],
+    "skillsDescription":"Full Stack Development",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"MySql"
+      }
+      
+    ],
+    // "portfolio":[
+    //   {
+    //     "name":"project1",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    // ],
+    "testimonials":[
+      {
+        "description":"Hey  I am Graphic Designer with 4 years of experience and i am glad to see this webiste made by my younger brother \"Amit Lokhande\" ",
+        "name":"Sumeet Lokhande"
+      },
+      {
+        "description":"So excited to see your wide future together",
+        "name":"Sharifa Shaikh"
+      }
+    ]
+  }
+  
+  export default resumeData
